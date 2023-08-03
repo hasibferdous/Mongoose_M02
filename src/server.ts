b745409973afff58c7cebbd1057975ee5fef5cc9
hasibferdous <hasibferdous.ict@gmail.com>
2023-08-04 01:25:19 +0600

    mongoose database connected

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const app = express()
+const port = 5000
+
+async function boot() {
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/mongoose-practice');
+        console.log("Database Connected");
+    }
+    catch(err){
+        console.log("Failed to connect",err);
+    }
+    }
+
+    boot()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
