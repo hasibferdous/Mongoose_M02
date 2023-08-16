f45b6453ef9643b8808888bc2c8ddb3593c49dab
hasibferdous <hasibferdous.ict@gmail.com>
2023-08-16 23:58:20 +0600

    import app from app.ts to server.ts

--- src/app.ts
+++ src/app.ts
@@ -9,3 +9,5 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello World!')
   })
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import app from "./app";
 
 const port = 5000
 
